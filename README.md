d47d3f70f741a69867905e95081ac469d83b2e29
kailous <kailous@live.cn>
2021-03-01 15:44:10 +0800

    完善一些界面的汉化。
    单页关于属性创建和设置的汉化内容

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Notion中文语言包
-这个不是官方的语言包，是一个chrome插件。修改的一个俄罗斯的本地化项目。目前汉化还不是很完全，感兴趣的同学可以一起维护。在官方的中文版正式发布前，可以先凑合用一下。
-[点此下载]: https://github.com/kailous/Notion-Language-Chinese/archive/v1.0.zip
-#notion #中文 #汉化 #插件
+# Notion-Language-Chinese
+这个不是官方的语言包，是一个chrome插件。修改的一个俄罗斯的本地化项目。目前汉化还不是很完全，感兴趣的同学可以一起维护。
---
+++ js/content.js
@@ -0,0 +1,678 @@
+var strings = {
+    //#通用
+    "Saving...": "保存…",
+    "Loading...": "下载…",
+    "New": "新的",
+    "Create": "创造",
+    "Remove": "删除",
+    "Select": "选择",
+    "Open": "打开",
+    "Clear": "清除",
+    "Cancel": "取消",
+    "Upload": "上传",
+    "Link": "链接",
+    "Submit": "提交",
+    "Update": "更新",
+    //#通用
+
+    //#
+    "Property Type": "属性类型",
+    "Basic": "基础",
+    "Number": "数字",
+    "Multi-select": "多选项",
+    "Date": "日期",
+    "Person": "人",
+    "Files & media": "文件和媒体",
+    "Checkbox": "复选框",
+    "Phone": "电话",
+    "Advanced": "高级",
+    "Formula": "公式",
+    "Relation": "关联",
+    "Rollup": "汇总",
+    "Created time": "创建时间",
+    "Created by": "创建人",
+    "Last edited time": "上次编辑时间",
+    "Last edited by": "上次编辑人",
+    //#
+    
+    //#
+    "Hide property": "隐藏属性",
+    "Always show": "始终显示",
+    "Hide when empty": "值为空时隐藏",
+    "Always hide": "总是隐藏",
+    "Duplicate property": "复制该属性",
+    "Delete property": "删除该属性",
+    //#
+    
+    //#region Login
+    "Sign up": "注册",
+    "Log in": "登录",
+
+    "Continue with Google": "用Google继续",
+    "Continue with Apple": "与Apple继续",
+
+    "Continue with email": "通过电子邮件继续",
+    "Create new account": "创建新帐户",
+    "Continue with password": "继续输入密码",
+
+    "Forgot password?": "忘记了密码？",
+    "You can also ": "您也可以 ",
+    "continue with SAML SSO": "继续使用SAML SSO",
+
+    "There is no existing account under this email address. We just sent you a temporary sign up code.": "此电子邮件地址下没有现有帐户。我们只是向您发送了一个临时注册代码。",
+    "Please check your inbox.": "请检查您的收件箱。",
+    "Sign up code": "注册代码",
+
+    "Password": "密码",
+    "Incorrect password.": "错误的密码。",
+    "Your login code was invalid, or expired.": "您的登录代码无效或已过期。",
+
+    //#endregion
+
+    //#region Onboarding
+
+    "Welcome to Notion": "欢迎使用Notion",
+    "First things first, tell us a bit about yourself.": "首先，首先要向我们介绍一些关于您自己的信息。",
+    "Set a password": "设置密码",
+    "You're currently logged in as ": "您当前登录为 ",
+    "Continue": "继续",
+
+    "How are you planning to use Notion?": "您打算如何使用Notion?",
+    "We’ll streamline your setup experience accordingly.": "我们将为您准备一个工作区。",
+    "For myself": "对于我自己",
+    "Write better. Think more clearly. Stay organized.": "写得更好。请更清晰地思考。保持井井有条。",
+    "Free for 1 person": "1人免费",
+    "With my team": "与我的团队",
+    "Wikis, docs, tasks & projects, all in one place.": "Wiki，文档，任务和项目都集中在一处。",
+    "Start for free": "免费开始",
+    "Take me to Notion": "带我去Notion!",
+
+    //#endregion
+
+    //#region Settings
+
+    //Sections
+    "My account": "我的帐户",
+    "My notifications": "我的通知",
+    "My connected apps": "已连接的应用程序",
+    "Language & region": "语言和地区",
+    "Appearance": "外观",
+    "Earn credit": "赚取奖金",
+    "Workspace": "工作区",
+    "Members": "成员",
+    "Upgrade": "升级",
+    "Plans": "计划",
+    "Billing": "结算",
+    "Security & identity": "安全与身份",
+
+    //#region 
+
+    "Account": "帐户",
+    "Changes to account settings will apply to all of your workspaces.": "对帐户设置的更改将应用​​于您的所有工作空间。",
+    "Learn more.": "了解更多。",
+    "Photo": "照片",
+    "Upload photo": "上传照片",
+    "Personal info": "个人信息",
+    "Email": "邮箱",
+    "Change email": "更改邮箱",
+    "First name": "名字",
+    "Last name": "姓氏",
+    "You can set a permanent password if you don't want to use temporary login codes.": "如果您不想使用临时登录代码，则可以设置永久密码。",
+    "Change password": "更改密码",
+    "Remove password": "删除密码",
+    "Danger zone": "危险区域",
+    "Delete my account": "删除我的帐户",
+
+    //#endregion
+
+    //#endregion
+
+    //#region Side toolbar
+    "Quick Find": "快速查找",
+    "Settings": "设置",
+    "Settings & Members": "设置和成员",
+    "All Updates": "所有更新",
+
+    "Shared": "共享",
+    "Private": "私人",
+    "Favorites": "收藏夹",
+    "Trash": "废物箱",
+    "New page": "新页面",
+    "Add a page": "添加页面",
+
+    "You are currently a guest. To see all the workspace pages, ask an admin to upgrade you to a member.": "您当前是来宾。要查看所有工作区页面，请要求管理员将您升级为成员。",
+    "Create a workspace": "创建工作空间",
+    //#endregion
+
+    //#region Top toolbar
+    "Share": "分享",
+    "Updates": "更新",
+    "Favorite": "收藏夹",
+    "Favorited": "收藏夹",
+    //#endregion
+
+    //#region Menu
+    "Database lock": "数据库锁定",
+    "Add to Favorites": "添加到收藏夹",
+    "Remove from Favorites": "从收藏夹中删除",
+    "Copy link": "复制链接",
+    "Open in Windows app": "在应用程序中打开",
+    "Undo": "撤消",
+    "Page history": "页面历史记录",
+    "Show deleted pages": "显示已删除的页面",
+    "Export": "导出",
+    "PDF, HTML, Markdown": "PDF, HTML, Markdown",
+    "Merge with CSV": "与CSV合并",
+    "Rename": "重命名",
+    "Edit property": "编辑属性",
+    "Move to": "移动到",
+    "Move to ": "移动到 ",
+    "Private pages": "私人页面",
+    "Comment": "评论",
+    "Turn into": "转换成",
+    "Turn into page": "转换成页面",
+    "Turn into inline": "转换成内联",
+    "Add as sub-page": "添加为子页面",
+    "Caption": "解说词",
+    "Wrap code": "包装代码",
+    "Dismiss": "消除",
+    "Mention": "提及",
+    "Color": "颜色",
+
+    "Style": "样式",
+    "Default": "默认",
+    "Serif": "衬线",
+    "Mono": "线性",
+    "Small text": "小文本",
+    "Full width": "全宽",
+    "Duplicate": "复制",
+    "Import": "导入",
+    "Customize page": "定制页面",
+    "Lock page": "锁定页面",
+    "Unlock page": "解锁页面",
+    //#endregion
+
+    //#region Share
+    "Share to web": "共享到网页",
+    "Publish and share link with anyone": "与任何人共享链接",
+
+    "Add people, groups, or emails": "添加人员，组或电子邮件",
+    "Add people": "添加人员",
+
+    "Allow editing": "允许编辑",
+    "Allow comments": "允许评论",
+    "Allow duplicate as template": "允许复制作为模板",
+    "Search engine indexing": "搜索引擎索引",
+
+    "Based on ": "基于 ",
+
+    "Full access": "完全访问权限",
+    "Can edit and share with others.": "可以与他人编辑和共享。",
+    "Can edit": "可以编辑",
+    "Can edit, but not share with others.": "可以编辑，但不能与他人共享。",
+    "Can comment": "可以发表评论",
+    "Can view and comment, but not edit.": "可以查看和评论，但不能编辑。",
+    "Can view": "可以查看",
+    "Cannot edit or share with others.": "无法编辑或与他人共享。",
+    "You’ll override permissions from the parent page if you change this role.": "如果更改此角色，您将覆盖从父页面获得的权限。",
+    //#endregion
+
+    //#region Updates
+    "Follow this page": "关注页面更改",
+    "Connect Slack channel": "连接Slack",
+
+    " in ": " 在 ",
+    " updated permission for ": " 更新了(а) 的权限",
+    " edited ": " 编辑(а) ",
+    " commented in ": " 评论了(а) ",
+    " locked ": " 锁定了(а) ",
+    " unlocked ": " 解锁了(а) ",
+    " created ": " 创建了(а) ",
+    " deleted ": " 删除了(а) ",
+    " edited view ": " 编辑了(а) 视图 ",
+    " created view ": " 创建了(а) 视图 ",
+    " deleted view ": " 删除了(а) 视图 ",
+    " created property ": " 创建了(а) 属性 ",
+    " edited property ": " 编辑了(а) 属性 ",
+    " deleted property ": " 删除了(а) 属性 ",
+    //#endregion
+
+    //#region Database
+    "Properties": "属性",
+    "Group by ": "分组于 ",
+    "Filter": "过滤器",
+    "Sort": "排序",
+    "Hide": "隐藏",
+    "Delete": "删除",
+    "Search": "搜索",
+    "Copy link to view": "复制查看链接",
+    "Templates": "范本",
+    "Group by": "更改分组",
+    "Hidden columns": "隐藏的列",
+    "Views for": "观看次数",
+    "Add a view": "添加视图",
+    "Add a group": "添加组",
+    "Wrap cells": "换行",
+
+    //#region Views
+    "Table": "表格",
+    "Table with types to store and view any kind of structured data": "具有用于存储和查看任何类型的结构化数据的类型的表",
+    "Board": "看板",
+    "Kanban board, great for project planning and bug tracking": "看板，非常适合项目计划和错误跟踪",
+    "Timeline": "时间轴",
+    "A timeline view, great for project scheduling and planning": "时间轴视图，非常适合项目的计划和计划”：“时间轴，非常适合项目的计划和计划",
+    "Calendar": "日历",
+    "Month view, for event planning and scheduling": "用于事件计划和安排的月视图",
+    "List": "列表",
+    "A simplified page view, great for bookmarks and notes": "简化的页面视图，非常适合书签和注释",
+    "Gallery": "画廊",
+    "Grid of cards, use for mood boards, index cards, and recipes": "纸牌网格，用于情绪板，索引卡和食谱",
+    "View name": "视图名称",
+    //#endregion
+
+    //#region Properties
+    "Properties for": "查看属性",
+
+    "Card preview": "卡片预览",
+    "Page cover": "页面封面",
+    "Page content": "页面内容",
+
+    "Card size": "卡片大小",
+    "Small": "小",
+    "Medium": "中",
+    "Large": "大",
+
+    "Add a property": "新属性",
+    //#endregion
+
+    //#region Filters
+    "Filters for": "过滤器",
+    "Add a filter": "添加过滤器",
+    "Add a filter group": "添加过滤器组",
+    "A group to nest more filters": "用于嵌套更多过滤器的组",
+    "Turn into group": "转换成群组",
+    "Turn into filter": "转换成过滤器",
+    "Wrap in group": "分组换行",
+    "Create a filter group around this": "为此过滤器创建分组",
+
+    "Where": "哪里",
+    "And": "和",
+    "Or": "或",
+
+    "Contains": "继续",
+    "Does not contain": "不包含",
+    "Is empty": "这是空的",
+    "Is not empty": "这不是空的",
+
+    "Pick a person": "挑选一个人",
+    "Me": "我",
+    //#endregion
+
+    //#region Sort
+    "Sort for": "排序",
+    "Ascending": "升序",
+    "Descending": "降序",
+    "Add a sort": "添加排序",
+    //#endregion
+
+    //#region 
+    "Templates for": "模板",
+    "New template": "新建模板",
+    "Empty page": "空页面",
+    //#endregion
+
+    //#endregion
+
+    //#region Page
+    "Open as page": "打开为页面",
+    "Add to": "添加到",
+
+    "Add description": "添加说明",
+    "Hide description": "隐藏说明",
+
+    "Add cover": "添加封面",
+    "Change cover": "更改封面",
+    "Reposition": "刷新",
+    "Save position": "保存位置",
+
+    //Cover dialog
+    "Choose an image": "选择图像",
+    "Images wider than 1500 pixels work best.": "大于1500像素的图像效果最好。",
+    "Works with any image from the web.": "使用来自网络的任何图像。",
+
+    //Icon
+    "Add icon": "添加图标",
+    "Upload an image": "上传图像",
+    "Random": "随机",
+    "Recommended size is 280 × 280 pixels": "建议尺寸为280×280像素",
+    "Upload an image": "上传图像",
+
+    //Comments
+    "Add comment": "添加注释",
+    "Send": "发送",
+    "Resolve": "解决",
+    "Edit comment": "编辑评论",
+    "Delete comment": "删除评论",
+    //#endregion
+
+    //#region Editor
+
+    //#region Blocks
+    "Basic blocks": "基本块",
+    "Inline": "内联",
+    "Media": "媒体",
+    "Database": "数据库",
+    "Embeds": "嵌入",
+    "Advanced blocks": "高级块",
+
+    "Text": "文本",
+    "Just start writing with plain text.": "就从纯文本开始吧。",
+
+    "Page": "页面",
+    "Embed a sub-page inside this page.": "在此页中嵌入子页。",
+
+    "To-do list": "待办事项列表",
+    "Track tasks with a to-do list.": "使用待办事项列表跟踪任务。",
+
+    "Heading 1": "标题 1",
+    "Big section heading.": "插入大标题段落。",
+
+    "Heading 2": "标题 2",
+    "Medium section heading.": "插入中标题段落。",
+
+    "Heading 3": "标题 3",
+    "Small section heading.": "插入小标题段落。",
+
+    "Bulleted list": "项目符号列表",
+    "Create a simple bulleted list.": "创建一个简单的项目符号列表。",
+
+    "Numbered list": "数字列表",
+    "Create a list with numbering.": "创建带有编号的列表。",
+
+    "Toggle list": "收缩列表",
+    "Toggles can hide and show content inside.": "创建可以切换隐藏和显示内部内容的列表。",
+
+    "Quote": "引用",
+    "Capture a quote.": "引用他人文章内容。",
+
+    "Divider": "分隔器",
+    "Visually divide blocks.": "直观地划分块。",
+
+    "Link to page": "链接到页面",
+    "Link to an existing page.": "链接到现有页面。",
+
+    "Callout": "标注",
+    "Make writing stand out.": "让文字脱颖而出。",
+
+    "Mention a person": "提到某人",
+    "Ping someone so they get a notification.": "Ping某人以便他们得到通知。",
+
+    "Mention a page": "提到某页",
+    "Mention a page and place in text.": "提及某个页面并放在文本中。",
+
+    "Date or reminder": "日期或提醒",
+    "Insert a date or reminder in text.": "在文本中插入日期或提醒。",
+
+    "Emoji": "表情符号",
+    "Search for an emoji to place in text.": "搜索要放置在文本中的表情符号。",
+
+    "Inline equation": "内联方程式",
+    "Insert mathematical symbols in text.": "在文本中插入数学符号。",
+
+    "Table - Inline": "表格-内联",
+    "Create a table inline in this page.": "在此页中创建内联表格。",
+
+    "Board - Inline": "看板-内联",
+    "Create a kanban board inline in this page.": "在该页中创建内联看板。",
+
+    "Gallery - Inline": "画廊-内联",
+    "Create a gallery inline in this page.": "在该页中创建内联画廊。",
+
+    "List - Inline": "列表-内联",
+    "Create a list inline in this page.": "在该页中创建内联列表。",
+
+    "Calendar - Inline": "日历-内联",
+    "Create a calendar inline in this page.": "在该页中创建内联日历。",
+
+    "Timeline - Inline": "时间线-内联",
+    "Create a timeline inline in this page.": "在该页中创建内联时间线。",
+
+    "Table - Full page": "表格-整页",
+    "Create a table as its own page.": "创建一个表格作为子页面。",
+
+    "Board - Full page": "看板-整页",
+    "Create a kanban board as its own page.": "创建一个看板作为子页面。",
+
+    "Gallery - Full page": "画廊-整页",
+    "Create a gallery as its own page.": "创建一个画廊作为子页面。",
+
+    "Calendar - Full page": "日历-整页",
+    "Create a calendar as its own page.": "创建一个日历作为子页面。",
+
+    "Timeline - Full page": "时间线-整页",
+    "Create a timeline as its own page.": "创建一个时间线作为子页面。",
+
+    "Create linked database": "创建链接数据库",
+    "Create custom views for a database.": "为数据库创建自定义视图。",
+
+    "Image": "图片",
+    "Upload or embed with a link.": "上传或嵌入链接。",
+
+    "Web bookmark": "Web书签",
+    "Save a link as a visual bookmark.": "将链接另存为可视书签。",
+
+    "Video": "视频",
+    "Embed from YouTube, Vimeo...": "嵌入YouTube, Vimeo...",
+
+    "Audio": "音频",
+    "Embed from SoundCloud, Spotify...": "嵌入SoundCloud, Spotify...",
+
+    "Code": "代码",
+    "Capture a code snippet.": "插入代码片段。",
+
+    "File": "文件",
+    "Upload or embed with a link.": "上传或嵌入链接。",
+
+
+    //#endregion
+
+    //#region Date-Time selector
+    "Remind": "提醒",
+
+    "None": "无",
+    "At time of event": "事件发生时",
+    "5 minutes before": "5分钟前",
+    "10 minutes before": "10分钟前",
+    "15 minutes before": "15分钟前",
+    "30 minutes before": "30分钟前",
+    "1 hour before": "1小时前",
+    "2 hours before": "2小时前",
+    "On day of event (9:00 AM)": "在当天的 (9:00)",
+    "1 day before (9:00 AM)": "在1天前的 (9:00)",
+    "2 days before (9:00 AM)": "在2天前的 (9:00)",
+    "1 week before (9:00 AM)": "在1周前的  (9:00)",
+
+    "End date": "结束日期",
+    "Include time": "包含时间",
+    "Date format & timezone": "日期格式和时区",
+
+    "Date format": "日期格式",
+    "Full date": "完整日期",
+    "Month/Day/Year": "月/日/年",
+    "Day/Month/Year": "日/月/年",
+    "Year/Month/Day": "年/月/日",
+    "Relative": "相对",
+
+    "Time format": "时间格式",
+    "12 hour": "12小时",
+    "24 hour": "24小时",
+
+    "Timezone": "时区",
+    //#endregion
+
+    //#endregion
+
+    //#Search
+    "Recent pages": "最近的页面",
+    "Open in a new tab": "在新选项卡中打开",
+    //#Tooltips
+    "Pin this page in your sidebar": "将此页固定在侧边栏中"
+}
+
+var placeholders = {
+    //#region Page
+    "Add a comment…": "添加评论…",
+    "Type '/' for commands": "键入命令 /",
+    "Untitled": "无标题",
+    "Press Enter to continue with an empty page, or pick a template (↑↓ to select)": "按Enter键继续空白页，或选择模板（↑↓选择）",
+    "Empty": "空的",
+    "Add a description…": "添加说明…",
+    //#endregion
+
+    "Type to search...": "输入要搜索的内容…",
+    "Enter your email address...": "输入您的邮箱地址…",
+    "Enter your password...": "输入您的密码…",
+    //"Paste login code": "Вставьте код регистрации",
+
+
+    "Add to another page...": "添加到另一页…",
+    "Filter actions…": "筛选操作…",
+
+    "Paste an image link…": "粘贴图像链接…",
+    "Search for an image…": "搜索图像…",
+    "Search for a person…": "搜索某人…",
+    "Type something…": "输入一些内容…",
+    "Filter...": "过滤…",
+    "Search for a page, or create a new one…": "搜索页面或创建新页面…",
+    "Add new page in…": "添加新页面到…",
+}
+
+var stringParts = {
+    "Today at": "今天"
+}
+
+function isNoTranslate(node) {
+    if (hasSomeParentTheAttribute(node, "role", "button")) {
+        console.log('isNoTranslate');
+        return false;
+    }
+
+    return true;
+}
+
+function hasSomeParentTheClass(element, classname) {
+    if (element.classList && element.classList.contains(classname)) return true; 
+    return element.parentNode && hasSomeParentTheClass(element.parentNode, classname);
+}
+
+function hasSomeParentTheAttribute(element, attributeName, attributeValue) {
+    if (element.nodeType === 1 && element.hasAttribute(attributeName) && element.getAttribute(attributeName) == attributeValue) return true; 
+    return element.parentNode && hasSomeParentTheAttribute(element.parentNode, attributeValue);
+}
+
+function translateNode(node) {
+    // if (node.nodeType === 1) {
+    //     console.log(node.classList);
+    //     console.log(node.innerText);
+    // }
+    // console.log(`node.classList.contains('notranslate'): ${node?.classList?.contains('notranslate')}`)
+    // console.log(`isNoTranslate(node): ${isNoTranslate(node)}`)
+    // console.log(`hasSomeParentTheClass(node, "notranslate"): ${hasSomeParentTheClass(node, "notranslate")}`)
+    if (node.nodeType === 1 && isNoTranslate(node) && hasSomeParentTheClass(node, "notranslate")) {
+        //console.log("this is page content, returning");
+        return;
+    }
+    // if (node.nodeType === 1 && ) {
+    //     console.log("returning because of the parent class");
+    //     return;
+    // }
+    if (node.nodeType === 1 && node.hasAttribute("placeholder")) {
+        translatePlaceholder(node);
+        return;
+    }
+
+    if (node.innerHTML in strings) {
+        var translated = strings[node.innerHTML];
+        console.log(`[innerHTML] translating node, old value: "${node.innerHTML}", new value: "${translated}"`)
+        node.innerHTML = translated;
+    }
+    //else пока нафиг ломает кучу элементов
+    // else {
+    //     //console.log(`Searching stringParts for innerHTML: ${node.innerHTML}`);
+    //     Object.keys(stringParts).forEach(function(key) {
+    //         //
+    //         //logic();
+    //         //console.log(`Key: ${key}`);
+    //         if (node.innerHTML != null && node.innerHTML.includes(key)) {
+    //             let translated = stringParts[key];
+    //             console.log(`[stringParts, innerHTML] translating node, old value: "${node.innerHTML}", new value: "${translated}"`)
+    //             node.innerHTML = node.innerHTML.replace(key, translated);
+    //         }
+    //       });
+    // }
+
+    if (node.data in strings) {
+        if (node.data != null) console.log(`node data: ${node.data}`);
+        var translated = strings[node.data];
+        console.log(`[data] translating node, old value: "${node.data}", new value: "${translated}"`)
+        node.data = translated;
+    };
+
+    if (node.childNodes != null) {
+        var nodes = node.childNodes;
+        for (var i = 0; i < nodes.length; i++) {
+            translateNode(nodes[i]);
+        }
+    }
+}
+
+function translatePlaceholder(node) {
+    let placeholderValue = node.getAttribute("placeholder");
+    console.log(`node placeholder: ${placeholderValue}`);
+    if (placeholderValue in placeholders) {
+        var translated = placeholders[placeholderValue];
+        console.log(`[placeholder] translating node, old value: "${placeholderValue}", new value: "${translated}"`)
+        node.setAttribute("placeholder", translated);
+    }
+}
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('notion-app');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function (mutationsList, observer) {
+
+    for (let mutation of mutationsList) {
+        if (mutation.type == "attributes") {
+            if (mutation.attributeName = "placeholder") continue;
+            //console.log(`Mutated attribute: ${mutation.attributeName}`);
+            translatePlaceholder(mutation.target);
+        }
+        //console.log(`Mutated! Type: ${mutation.type}`);
+        if (mutation.type == "childList") {
+            for (let node of mutation.addedNodes) {
+                translateNode(node);
+            }
+        }
+        if (mutation.type == "characterData") {
+            translateNode(mutation.target);
+        }
+
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback, {
+    childList: true, // наблюдать за непосредственными детьми
+    subtree: true,// и более глубокими потомками
+    attributeFilter: ['placeholder'],
+    characterData: true,
+}
+);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
